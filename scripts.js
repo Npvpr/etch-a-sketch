c371e2108b9c6fb58a0913f815809e26661fb47e
unknown <nlmnever2@gmail.com>
2023-01-04 12:28:57 +0630

    day 2

--- scripts.js
+++ scripts.js
@@ -7,5 +7,6 @@ for(let i = 0; i < 6; i++){
     pixels.push(document.createElement("div"));
     pixels[i].innerText = i;
     pixels[i].style.border = "solid 1px"
+    pixels[i].style.backgroundColor = "black";
     drawCon.appendChild(pixels[i])
 }
